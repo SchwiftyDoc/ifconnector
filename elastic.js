ab6178ff70c2e2fd0db536d171633e8e61988ca8
SchwiftyDoc <cs1first2>
2017-11-21 08:17:41 +0000

    Test: getConnections() - Correction elastic.js

--- elastic.js
+++ elastic.js
@@ -3,21 +3,23 @@ const http = require('http');
 
 exports.Elastic = class {
 
-    // Set Request Options
-    static options = {
-        host: config.elasticsearch.host,
-        port: config.elasticsearch.port,
-        path: config.elasticsearch.index
-            + '/' + config.elasticsearch.type,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8"
-            //"Content-Length": Buffer.byteLength(data)
-        }
-    };
 
     constructor(data) {
+
+        // Set data and HTTP Request options
         this.data = data;
+        const options = {
+            host: config.elasticsearch.host,
+            port: config.elasticsearch.port,
+            path: config.elasticsearch.index
+                + '/' + config.elasticsearch.type,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8",
+                "Content-Length": Buffer.byteLength(this.data)
+            }
+        };
+
         // Set request and handle answers
         const req = http.request(options, (res) => {
             res.on('error', (err) => {

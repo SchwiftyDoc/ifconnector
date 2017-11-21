7c2fe318446c28d95c52057af733349f2f178434
SchwiftyDoc <cs1first2>
2017-11-21 10:53:31 +0000

    Datafile Poller

--- iftop.js
+++ iftop.js
@@ -9,27 +9,7 @@ exports.Iftop = class {
     constructor() {
         this.connections = [];
         this.datafile = new Datafile();
-        this.execute = child.exec('iftop -i ' + config.iftop.interface + ' -t -n -s ' + config.iftop.duration + ' > ' + this.datafile.file)
-        this.execute.on('close', this.close);
-
-    }
-
-    close(code) {
-
-        if(code != 0) {
-            console.error('iftop has stopped unexpectidly on code : ' + code);
-            process.exit(code);
-        }
-
-        console.log(this.datafile.getConnections());
-        process.exit(0);
-
-        // Send to Elastic search
-        const elastic = new Elastic(this);
-
-        // Delete raw data?
-        if (!config.data.keep)
-            this.datafile.remove();
+        this.execute = child.exec('iftop -i ' + config.iftop.interface + ' -t -n -s ' + config.iftop.duration + ' > ' + this.datafile.file);
     }
 
 };

bdde6ea1b0b9538b5b522cd1f0ea0ab62b88d255
SchwiftyDoc <cs1first2>
2017-11-22 10:00:28 +0000

    Bandwidth correction : test6

--- datafile.js
+++ datafile.js
@@ -34,6 +34,7 @@ class Datafile {
             str2 = str2.replace(/\s\s+/g, ' ');
             str2 = str2.split(' ');
 
+
             json = {
                 'source': str1[1],
                 'destination': str2[0],
